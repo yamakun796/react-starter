6071bfa4934ab8065959f2eb8cfdeca02e53641d
utain <utain.wit@dohome.co.th>
2022-12-01 00:13:26 +0700

    react starter

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import ExamplePage from './pages/example/ExamplePage'
+import './styles/app.scss'
+
+function App() {
+  return (
+    <div>
+      <ExamplePage />
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/example/ExamplePage.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ExampleComponent from '../../components/example/ExampleComponent'
+
+export default function ExamplePage() {
+  return (
+    <div>
+      <ExampleComponent />
+    </div>
+  )
+}
